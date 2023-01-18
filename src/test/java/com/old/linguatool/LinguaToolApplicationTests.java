c05df1b09c54fcb6570b1bbb1b5bbdf4595199ed
Oleh_Kuzan <Oleh_Kuzan@epam.com>
2023-01-18 21:28:56 +0200

    Tests are passing

--- src/test/java/com/old/linguatool/LinguaToolApplicationTests.java
+++ src/test/java/com/old/linguatool/LinguaToolApplicationTests.java
@@ -1,9 +1,15 @@
 package com.old.linguatool;
 
+import com.linguatool.LinguaToolApplication;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
+@SpringBootTest(classes = {LinguaToolApplication.class})
+@ExtendWith(SpringExtension.class)
 class LinguaToolApplicationTests {
 
     @Test
