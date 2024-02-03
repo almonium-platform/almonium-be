4f433b639c15356e2bd2c5dd057d93f27e318421
okuzan <kuzanoleg@gmail.com>
2024-02-03 23:17:11 +0200

    Added app context loads text, added needed configurations

---
+++ src/test/java/com/linguarium/ApplicationContextLoadTest.java
@@ -0,0 +1,11 @@
+package com.linguarium;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ApplicationContextLoadTest {
+    @Test
+    void contextLoads() {
+    }
+}
