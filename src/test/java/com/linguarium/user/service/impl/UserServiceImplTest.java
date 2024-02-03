7121912bcd0a6e4f1dee41a70a0da87a55b47fc8
okuzan <kuzanoleg@gmail.com>
2024-02-03 21:37:48 +0200

    added imports to checkstyle as in Intellij IDEA

--- src/test/java/com/linguarium/user/service/impl/UserServiceImplTest.java
+++ src/test/java/com/linguarium/user/service/impl/UserServiceImplTest.java
@@ -533,16 +533,16 @@ class UserServiceImplTest {
         verifyNoInteractions(authenticationManager);
     }
 
-    private Map<String, Object> createAttributes(String email, String userId) {
-        return createAttributes(email, userId, PROFILE_PIC_LINK);
-    }
-
     private void mockTags(Set<Long> tagIds, User user) {
         when(cardTagRepository.getLearnersTags(user.getLearner())).thenReturn(tagIds);
         when(tagRepository.findById(anyLong())).thenAnswer(invocation -> {
             Long tagId = invocation.getArgument(0);
             return Optional.of(new Tag(tagId, "Tag " + tagId));
         });
+    }
+
+    private Map<String, Object> createAttributes(String email, String userId) {
+        return createAttributes(email, userId, PROFILE_PIC_LINK);
     }
 
     private Map<String, Object> createAttributes(String email, String userId, String profilePicUrl) {
