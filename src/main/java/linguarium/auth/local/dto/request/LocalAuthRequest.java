c93a4d3b8d2b13ec14d8def1f6fba4f98beaf0f8
okuzan <kuzanoleg@gmail.com>
2024-05-30 21:41:33 +0300

    local auth request validation

--- src/main/java/linguarium/auth/local/dto/request/LocalAuthRequest.java
+++ src/main/java/linguarium/auth/local/dto/request/LocalAuthRequest.java
@@ -1,6 +1,15 @@
 package linguarium.auth.local.dto.request;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
-public record LocalAuthRequest(@NotBlank String email, @NotBlank String password) {
+public record LocalAuthRequest(
+        @NotBlank(message = "Email cannot be blank")
+        @Email
+        String email,
+
+        @NotBlank(message = "Password cannot be blank")
+        @Size(min = 8, message = "Password must be at least 8 characters long")
+        String password) {
 }
