5fd9d5e243d3ab5d602703dff9895f0ef9294f65
okuzan <kuzanoleg@gmail.com>
2024-06-03 21:00:34 +0300

    refined email messages

--- src/main/java/linguarium/util/email/service/EmailComposerService.java
+++ src/main/java/linguarium/util/email/service/EmailComposerService.java
@@ -8,9 +8,17 @@ import org.springframework.stereotype.Service;
 @Service
 public class EmailComposerService {
     private static final String EMAIL_VERIFICATION_SUBJECT = "Verify your email address";
-    private static final String EMAIL_VERIFICATION_BODY = "Please verify your email by clicking the following link: %s";
     private static final String PASSWORD_RESET_SUBJECT = "Reset your password";
-    private static final String PASSWORD_RESET_BODY = "To reset your password, click the following link: %s";
+    private static final String EMAIL_VERIFICATION_BODY = """
+            Please verify your email by clicking the following link: %s.
+            If the link doesn't work, please paste the code into the form provided.
+            If you didn't create an account, you can safely ignore this email.
+            """;
+    private static final String PASSWORD_RESET_BODY = """
+            To reset your password, click the following link: %s.
+            If the link doesn't work, please copy and paste it into your browser.
+            If you didn't request a password reset, you can safely ignore this email.
+            """;
 
     @Value("${app.server.domain}")
     private String domain;
