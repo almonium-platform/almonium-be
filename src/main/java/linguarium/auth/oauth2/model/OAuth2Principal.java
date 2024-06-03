4dce6bd67da8dbe2690e5b6dbf1cc5030d32f38a
okuzan <kuzanoleg@gmail.com>
2024-06-03 21:18:29 +0300

    removed unused constructor

--- src/main/java/linguarium/auth/oauth2/model/OAuth2Principal.java
+++ src/main/java/linguarium/auth/oauth2/model/OAuth2Principal.java
@@ -7,9 +7,7 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import linguarium.auth.common.enums.AuthProviderType;
 import linguarium.auth.common.model.entity.Principal;
-import linguarium.user.core.model.entity.User;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -29,13 +27,6 @@ import org.springframework.security.oauth2.core.user.OAuth2User;
 @DiscriminatorValue("OAUTH2")
 public class OAuth2Principal extends Principal implements OAuth2User {
     String providerUserId;
-
-    public OAuth2Principal(
-            User user, String email, AuthProviderType provider, String providerUserId, Map<String, Object> attributes) {
-        super(user, email, provider);
-        this.providerUserId = providerUserId;
-        this.attributes = attributes;
-    }
 
     @Transient
     @Builder.Default
