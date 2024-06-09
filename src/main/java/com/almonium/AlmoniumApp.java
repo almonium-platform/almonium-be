46602695a2ed17aba2b327621fde2eb4ff601484
okuzan <kuzanoleg@gmail.com>
2024-06-09 23:34:16 +0300

    main class renamed

--- src/main/java/com/almonium/AlmoniumApp.java
+++ src/main/java/com/almonium/AlmoniumApp.java
@@ -5,8 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class LinguaApp extends SpringBootServletInitializer {
+public class AlmoniumApp extends SpringBootServletInitializer {
     public static void main(String[] args) {
-        SpringApplication.run(LinguaApp.class, args);
+        SpringApplication.run(AlmoniumApp.class, args);
     }
 }
