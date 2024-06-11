26a4ed830861452d0d253d78fd21460c2a3ad3f9
okuzan <kuzanoleg@gmail.com>
2024-06-11 21:34:29 +0300

    email provider changed from gmail to zoho

--- src/main/java/com/almonium/util/email/service/EmailService.java
+++ src/main/java/com/almonium/util/email/service/EmailService.java
@@ -5,19 +5,24 @@ import static lombok.AccessLevel.PRIVATE;
 import com.almonium.util.email.dto.EmailDto;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-@FieldDefaults(makeFinal = true, level = PRIVATE)
+@FieldDefaults(level = PRIVATE)
 public class EmailService {
-    JavaMailSender mailSender;
+    final JavaMailSender mailSender;
+
+    @Value("${spring.mail.username}")
+    String from;
 
     public void sendEmail(EmailDto emailDto) {
         SimpleMailMessage message = new SimpleMailMessage();
         message.setTo(emailDto.recipient());
+        message.setFrom(from);
         message.setSubject(emailDto.subject());
         message.setText(emailDto.body());
         mailSender.send(message);
