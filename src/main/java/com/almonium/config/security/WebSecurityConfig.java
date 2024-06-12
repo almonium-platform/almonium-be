cdcbb962c02986c10e8d6e4e5c6dc441103e0cc6
okuzan <kuzanoleg@gmail.com>
2024-06-13 00:56:17 +0300

    cors apple

--- src/main/java/com/almonium/config/security/WebSecurityConfig.java
+++ src/main/java/com/almonium/config/security/WebSecurityConfig.java
@@ -73,7 +73,7 @@ public class WebSecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of(domain));
+        configuration.setAllowedOrigins(List.of(domain, "https://appleid.apple.com"));
         configuration.setAllowedMethods(List.of(GET, POST, PUT, DELETE, OPTIONS));
         configuration.setAllowedHeaders(List.of(CONTENT_TYPE, AUTHORIZATION, CACHE_CONTROL));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
