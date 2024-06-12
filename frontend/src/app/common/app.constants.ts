296284fe557cbb0e2d93bca24442f03f8701c499
okuzan <kuzanoleg@gmail.com>
2024-06-13 00:52:39 +0300

    apple

--- frontend/src/app/common/app.constants.ts
+++ frontend/src/app/common/app.constants.ts
@@ -10,7 +10,7 @@ export class AppConstants {
   public static LANG_API = AppConstants.API_URL + '/lang';
 
   private static OAUTH2_URL = AppConstants.API_URL + '/oauth2/authorization';
-  private static REDIRECT_URL = '?redirect_uri=http://localhost:9999/login';
+  private static REDIRECT_URL = '?redirect_uri=https://almonium.com/login';
 
   public static GOOGLE_AUTH_URL = AppConstants.OAUTH2_URL + '/google' + AppConstants.REDIRECT_URL;
   public static FACEBOOK_AUTH_URL = AppConstants.OAUTH2_URL + '/facebook' + AppConstants.REDIRECT_URL;
