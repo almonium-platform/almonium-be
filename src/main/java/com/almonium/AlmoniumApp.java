a3281b13eedfa7c30ef95b32f58013860d8f15bd
okuzan <kuzanoleg@gmail.com>
2024-06-15 13:27:43 +0300

    main class simplified

--- src/main/java/com/almonium/AlmoniumApp.java
+++ src/main/java/com/almonium/AlmoniumApp.java
@@ -2,10 +2,9 @@ package com.almonium;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class AlmoniumApp extends SpringBootServletInitializer {
+public class AlmoniumApp {
     public static void main(String[] args) {
         SpringApplication.run(AlmoniumApp.class, args);
     }
