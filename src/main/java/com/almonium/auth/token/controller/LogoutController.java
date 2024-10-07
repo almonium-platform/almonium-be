c8c7061938445648d02251eda42eb6557001f370
okuzan <kuzanoleg@gmail.com>
2024-10-07 23:52:05 +0300

    added public logout endpoint

---
+++ src/main/java/com/almonium/auth/token/controller/LogoutController.java
@@ -0,0 +1,26 @@
+package com.almonium.auth.token.controller;
+
+import static lombok.AccessLevel.PRIVATE;
+
+import com.almonium.auth.token.service.impl.AuthTokenService;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/public/auth")
+@RequiredArgsConstructor
+@FieldDefaults(level = PRIVATE, makeFinal = true)
+public class LogoutController {
+    AuthTokenService authTokenService;
+
+    @PostMapping("/logout")
+    public ResponseEntity<?> logoutPublic(HttpServletResponse response) {
+        authTokenService.clearTokenCookies(response);
+        return ResponseEntity.ok().build();
+    }
+}
