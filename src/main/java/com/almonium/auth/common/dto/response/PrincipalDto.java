23885b6634e6c96ea8f850f2ab1665a1087b408e
okuzan <kuzanoleg@gmail.com>
2024-11-03 21:36:45 +0200

    spotless

--- src/main/java/com/almonium/auth/common/dto/response/PrincipalDto.java
+++ src/main/java/com/almonium/auth/common/dto/response/PrincipalDto.java
@@ -3,4 +3,5 @@ package com.almonium.auth.common.dto.response;
 import com.almonium.auth.common.model.enums.AuthProviderType;
 import java.time.Instant;
 
-public record PrincipalDto(AuthProviderType provider, boolean emailVerified, String email, Instant createdAt) {}
+public record PrincipalDto(
+        AuthProviderType provider, boolean emailVerified, String email, Instant createdAt, Instant updatedAt) {}
