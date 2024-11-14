ba823630cd48dfebbf0f4cb41ee3ba5511c0d047
okuzan <kuzanoleg@gmail.com>
2024-11-14 18:12:26 +0200

    PlanLimitations constants

---
+++ src/main/java/com/almonium/subscription/constant/PlanLimitations.java
@@ -0,0 +1,6 @@
+package com.almonium.subscription.constant;
+
+public class PlanLimitations {
+    public static final int MAX_FLUENT_LANGS = 3;
+    public static final int MAX_TARGET_LANGS = 3;
+}
--- src/main/java/com/almonium/user/core/dto/LanguageSetupRequest.java
+++ src/main/java/com/almonium/user/core/dto/LanguageSetupRequest.java
@@ -1,9 +1,11 @@
 package com.almonium.user.core.dto;
 
 import com.almonium.analyzer.translator.model.enums.Language;
+import com.almonium.subscription.constant.PlanLimitations;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
 import java.util.Set;
 
 public record LanguageSetupRequest(
-        @NotEmpty @Size(max = 3) Set<Language> fluentLangs, @NotEmpty @Size(max = 3) Set<Language> targetLangs) {}
+        @NotEmpty @Size(max = PlanLimitations.MAX_FLUENT_LANGS) Set<Language> fluentLangs,
+        @NotEmpty @Size(max = PlanLimitations.MAX_TARGET_LANGS) Set<Language> targetLangs) {}
