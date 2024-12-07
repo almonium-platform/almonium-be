b4c70ba561083ac970d34b8faba9caa2393f33d8
okuzan <kuzanoleg@gmail.com>
2024-12-08 00:03:52 +0200

    logo update, several options

--- src/main/java/com/almonium/infra/email/service/EmailService.java
+++ src/main/java/com/almonium/infra/email/service/EmailService.java
@@ -24,7 +24,7 @@ import org.springframework.stereotype.Service;
 @FieldDefaults(level = PRIVATE)
 public class EmailService {
     private static final String ALMONIUM_FROM_FORMAT = "Almonium <%s>";
-    private static final String STATIC_LOGO_IMG = "static/logo-white-bg.png";
+    private static final String STATIC_LOGO_IMG = "static/logo-white-2.png";
     private static final String STATIC_TITLE_IMG = "static/title-white.png";
     private static final Map<String, String> EMAIL_ASSETS = Map.of(
             "logoHeader", STATIC_LOGO_IMG,
