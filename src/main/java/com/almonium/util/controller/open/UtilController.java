3785b8db5773ba9d0d2208728bcb4e9ea8e9da36
okuzan <kuzanoleg@gmail.com>
2024-12-08 00:04:11 +0200

    util controller - /public

--- src/main/java/com/almonium/util/controller/open/UtilController.java
+++ src/main/java/com/almonium/util/controller/open/UtilController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/util")
+@RequestMapping("/public/util")
 @RequiredArgsConstructor
 @FieldDefaults(level = PRIVATE, makeFinal = true)
 public class UtilController {
