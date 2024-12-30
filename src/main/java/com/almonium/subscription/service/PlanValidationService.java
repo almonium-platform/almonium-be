00ab34f8317296566586865e6ca28738feeb4461
okuzan <kuzanoleg@gmail.com>
2024-12-30 21:22:36 +0200

    minor, FieldDefaults

--- src/main/java/com/almonium/subscription/service/PlanValidationService.java
+++ src/main/java/com/almonium/subscription/service/PlanValidationService.java
@@ -1,17 +1,21 @@
 package com.almonium.subscription.service;
 
+import static lombok.AccessLevel.PRIVATE;
+
 import com.almonium.subscription.exception.PlanValidationException;
 import com.almonium.subscription.model.entity.Plan;
 import com.almonium.subscription.model.entity.PlanLimit;
 import com.almonium.subscription.model.entity.enums.PlanFeature;
 import com.almonium.user.core.model.entity.User;
 import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@FieldDefaults(makeFinal = true, level = PRIVATE)
 public class PlanValidationService {
-    private final PlanSubscriptionService subscriptionService;
+    PlanSubscriptionService subscriptionService;
 
     public void validatePlanFeature(User user, PlanFeature featureKey, int requestedValue) {
         Plan activePlan = subscriptionService.getActivePlan(user);
