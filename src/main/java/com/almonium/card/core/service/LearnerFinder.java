e27dba2682a0d2668ec8317686c349521856c678
okuzan <kuzanoleg@gmail.com>
2025-01-11 19:51:04 +0200

    fixed 500 lazy init

--- src/main/java/com/almonium/card/core/service/LearnerFinder.java
+++ src/main/java/com/almonium/card/core/service/LearnerFinder.java
@@ -1,17 +1,25 @@
 package com.almonium.card.core.service;
 
+import static lombok.AccessLevel.PRIVATE;
+
 import com.almonium.analyzer.translator.model.enums.Language;
 import com.almonium.user.core.model.entity.Learner;
 import com.almonium.user.core.model.entity.User;
+import com.almonium.user.core.repository.LearnerRepository;
 import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
+@FieldDefaults(makeFinal = true, level = PRIVATE)
 public class LearnerFinder {
+    LearnerRepository learnerRepository;
+
     public Learner findLearner(User user, Language language) {
-        return user.getLearners().stream()
-                .filter(learner -> learner.getLanguage().equals(language))
-                .findFirst()
+        return learnerRepository
+                .findByUserIdAndLanguage(user.getId(), language)
                 .orElseThrow(() -> new EntityNotFoundException("Learner not found for language: " + language));
     }
 }
