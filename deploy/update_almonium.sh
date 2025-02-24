068d3ab840781f1c1a8ca25db3b24bd9c9af6be9
okuzan <kuzanoleg@gmail.com>
2025-02-24 22:58:51 +0200

    zero-time deployment

--- deploy/update_almonium.sh
+++ deploy/update_almonium.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 cd /home/kuzanoleg/almonium
+
+# Pull latest code
 git reset --hard
 git clean -fd
 git pull
-sudo systemctl restart almonium.service
+
+# Generate a unique deploy version (timestamp-based)
+export DEPLOY_VERSION=$(date +%Y%m%d%H%M)
+echo "Deploying version: $DEPLOY_VERSION"
+
+# Build and start with the new version
+docker-compose build --no-cache
+docker-compose up -d --no-deps --build
+
+# Wait for the new version to start
+sleep 10
+
+# Remove old container
+docker image prune -f
