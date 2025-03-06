17593a8a3dd6251fddcda4eef0efbf6d8d263aac
okuzan <kuzanoleg@gmail.com>
2025-03-06 19:10:14 +0200

    fixed deprecated method

--- src/main/java/com/almonium/util/GeneralUtils.java
+++ src/main/java/com/almonium/util/GeneralUtils.java
@@ -7,7 +7,7 @@ import org.springframework.web.util.UriComponentsBuilder;
 @UtilityClass
 public class GeneralUtils {
     public String queryBuilder(String httpUrl, Collection<String> params) {
-        UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(httpUrl);
+        UriComponentsBuilder builder = UriComponentsBuilder.fromUriString(httpUrl);
         params.forEach(param -> builder.queryParam(param, "{" + param + "}"));
         return builder.encode().toUriString();
     }
