477a62849bea7f4e0dbff404eda1bb53d866da4d
okuzan <kuzanoleg@gmail.com>
2025-06-05 19:01:42 +0300

    fix spotless ktlint

--- src/main/java/com/almonium/config/AppConfig.kt
+++ src/main/java/com/almonium/config/AppConfig.kt
@@ -1,6 +1,15 @@
 package com.almonium.config
 
-import com.almonium.config.properties.*
+import com.almonium.config.properties.AiProperties
+import com.almonium.config.properties.AppProperties
+import com.almonium.config.properties.AppleOAuthProperties
+import com.almonium.config.properties.AppleOAuthProviderProperties
+import com.almonium.config.properties.ExternalApiProperties
+import com.almonium.config.properties.GoogleProperties
+import com.almonium.config.properties.MailProperties
+import com.almonium.config.properties.RabbitMQProperties
+import com.almonium.config.properties.StreamProperties
+import com.almonium.config.properties.StripeProperties
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.EnableAspectJAutoProxy
