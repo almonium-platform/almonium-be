12d56a7a09a190f063f769cc6f03ceb48f3c891c
okuzan <kuzanoleg@gmail.com>
2025-10-30 22:37:14 +0200

    User entity fixes. List to Set, orphanRemoval

--- src/main/java/com/almonium/user/core/mapper/LearnerMapper.java
+++ src/main/java/com/almonium/user/core/mapper/LearnerMapper.java
@@ -3,11 +3,12 @@ package com.almonium.user.core.mapper;
 import com.almonium.user.core.dto.response.LearnerDto;
 import com.almonium.user.core.model.entity.Learner;
 import java.util.List;
+import java.util.Set;
 import org.mapstruct.Mapper;
 
 @Mapper
 public interface LearnerMapper {
     LearnerDto toDto(Learner learner);
 
-    List<LearnerDto> toDto(List<Learner> learners);
+    List<LearnerDto> toDto(Set<Learner> learners);
 }
