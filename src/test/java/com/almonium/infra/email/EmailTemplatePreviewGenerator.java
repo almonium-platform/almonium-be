0f053508153555c275a33fb25bbecfc3b4d4daa9
okuzan <kuzanoleg@gmail.com>
2026-08-24 00:24:54 +0300

    fix

--- src/test/java/com/almonium/infra/email/EmailTemplatePreviewGenerator.java
+++ src/test/java/com/almonium/infra/email/EmailTemplatePreviewGenerator.java
@@ -21,9 +21,9 @@ import java.util.stream.Collectors;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.web.client.RestTemplate;
 
 /**
